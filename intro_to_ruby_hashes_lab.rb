3d739314f65765f87bd34c2500e9800be1abbefe
Daiane Bushey <daianebushey@gmail.com>
2018-01-03 01:58:04 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -56,8 +56,8 @@ def monopoly_with_fourth_tier
       :two_pieces_owned=>50,
       :three_pieces_owned=>100,
       :four_pieces_owned=>200
-			binding.pry
     },
+		binding.pry
     :names=>
     {
       :reading_railroad=>
